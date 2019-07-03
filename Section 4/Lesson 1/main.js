46e71c87c409435186c98ca7e0cce521a71a9065
Nikola Obradovic <obradovicnik01@gmail.com>
2019-07-03 16:57:37 +0200

    IIFE

---
+++ Section 4/Lesson 1/main.js
@@ -0,0 +1,22 @@
+// IIFE
+
+// function game() {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5);
+// }
+
+// game();
+
+
+
+// this is an IIFE :thonk: 
+(function () {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+
+
+(function (goodLuck) {
+  var score = Math.random() * 10;
+  console.log(score >= 5 - goodLuck);
+})(5);
